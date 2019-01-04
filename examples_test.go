815f19af6bc90f230cf135744d8fdb8760b286b7
Diego Cena <diego.cena@gmail.com>
2019-01-04 19:47:02 -0300

    improved doc

---
+++ examples_test.go
@@ -0,0 +1,50 @@
+package easyrabbit_test
+
+import (
+	"github.com/diegohce/easyrabbit"
+)
+
+func ExampleConnection_Publish() {
+
+	erc, err := easyrabbit.New("amqp://guest:guest@localhost/demo")
+	if err != nil {
+		// do something with err
+	}
+	defer erc.Close()
+
+	messageBody := "Hello, World!"
+
+	erc.Publish("myExchangeName", "easyrabbit pub demo", []byte(message))
+}
+
+func ExampleConnection_SetContentType() {
+
+	erc, err := easyrabbit.New("amqp://guest:guest@localhost/demo")
+	if err != nil {
+		// do something with err
+	}
+	defer erc.Close()
+
+	erc.SetContentType("application/json")
+}
+
+func ExampleConnection_ConsumeWithCallback() {
+
+	erc, err := easyrabbit.New("amqp://guest:guest@localhost/demo")
+	if err != nil {
+		// do something with err
+	}
+	defer erc.Close()
+
+	// Assuming messageCallback is defined as:
+	//func messageCallback(body []byte) error {
+	//	fmt.Println(string(body))
+	//}
+	stopConsumer, err := erc.ConsumeWithCallback("myQueueName", "easyrabbit examples", messageCallback)
+	if err != nil {
+		// do something with err
+	}
+
+	// To stop consumer
+	stopConsumer <- true
+}
